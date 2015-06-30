1cf6a0b5f14fe888760c404705d8a16531665955
周志康 <zhikangzhou@icloud.com>
2015-06-30 16:18:18 +0800

    update4

--- Cherry/app/src/main/java/cherry/activity/TagActivity.java
+++ Cherry/app/src/main/java/cherry/activity/TagActivity.java
@@ -1,41 +1,81 @@
 package cherry.activity;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cherry.cherry.R;
+import cherry.widget.Tag;
+import cherry.widget.TagListView;
+import cherry.widget.TagView;
 
 public class TagActivity extends Activity {
+    private TextView mTagDone;
+
+    private TagListView mTagListView;
+    private final List<Tag> mTags = new ArrayList<Tag>();
+    private final String[] titles = {"安全必备", "音乐", "父母学", "上班族必备",
+            "360手机卫士", "QQ", "输入法", "微信", "最美应用", "AndevUI", "蘑菇街"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tag);
+        setContentView(R.layout.select_tag_activity);
+        initView();
+        initEvent();
+    }
+
+    void initView(){
+        mTagListView = (TagListView) findViewById(R.id.tagview);
+        setUpData();
+        mTagListView.setTags(mTags);
+        mTagDone=(TextView)findViewById(R.id.tag_done);
+    }
+
+    void initEvent(){
+        mTagListView.setOnTagClickListener(new TagListView.OnTagClickListener() {
+            @Override
+            public void onTagClick(TagView tagView, Tag tag) {
+                if (tag.isChecked() == false) {
+                    tagView.setTextColor(Color.parseColor("#ffffff"));
+                    tagView.setBackgroundResource(R.drawable.tag_bg2);
+                    tag.setChecked(true);
+                } else if (tag.isChecked() == true) {
+                    tagView.setTextColor(Color.parseColor("#009688"));
+                    tagView.setBackgroundResource(R.drawable.tag_bg);
+                    tag.setChecked(false);
+                }
+
+            }
+        });
+        mTagDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tag, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void setUpData() {
+        for (int i = 0; i < 10; i++) {
+            Tag tag = new Tag();
+            tag.setId(i);
+            tag.setChecked(false);
+            tag.setTitle(titles[i]);
+            mTags.add(tag);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
